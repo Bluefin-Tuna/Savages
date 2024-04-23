b8d670c7d3a6e8d41bbe5e8d348b4c30dde4721d
Yuan <byuan48@aelscml19008g.ae.gatech.edu>
2024-04-23 16:21:20 -0400

    new books

--- Bo/data/txtToJsonl.py
+++ Bo/data/txtToJsonl.py
@@ -11,6 +11,7 @@ nltk.download('punkt')  # Ensure the 'punkt' tokenizer is downloaded
 
 # Get the author and title 
 def extract_author_title(file_path):
+
     with open(file_path, 'r', encoding='utf-8') as file:
         lines = file.readlines()
 
